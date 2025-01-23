04b6baa93cfabf192ed23728f26ea1109a6ed965
Dayan A <codayanabdulla@gmail.com>
2025-01-23 08:41:27 -0500

    Initial Commit

---
+++ RTR Challenge 5/RTR_Challenge_5App.swift
@@ -0,0 +1,17 @@
+//
+//  RTR_Challenge_5App.swift
+//  RTR Challenge 5
+//
+//  Created by Dayan Abdulla on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct RTR_Challenge_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
