ec47b5757ed1dcf0076ded9fba0829cdc6705868
Dayan A <codayanabdulla@gmail.com>
2025-01-24 08:48:05 -0500

    first fix

--- RTR Challenge 5/ContentView.swift
+++ RTR Challenge 5/ContentView.swift
@@ -9,15 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Game Menu")
+                .font(.title)
+                .padding()
+
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color.gray)
+                .frame(width: 200, height: 50)
+                .overlay(
+                    Text("Resume")
+                        .foregroundColor(.white)
+                )
+                .onTapGesture {
+              
+                    print("Start Game button tapped")
+                }
+
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color.gray)
+                .frame(width: 200, height: 50)
+                .overlay(
+                    Text("Settings")
+                        .foregroundColor(.white)
+                )
+                .onTapGesture {
+                    
+                    print("Settings button tapped")
+                }
+
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color.gray)
+                .frame(width: 200, height: 50)
+                .overlay(
+                    Text("High Scores")
+                        .foregroundColor(.white)
+                )
+                .onTapGesture {
+                    print("High Scores button tapped")
+                }
+
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.gray)
+                .frame(width: 200, height: 50)
+                .overlay(
+                    Text("Exit")
+                        .foregroundColor(.white)
+                )
+                .onTapGesture {
+                    print("Exit button tapped")
+                }
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
