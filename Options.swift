ee25f1c8d3d8934afc20c06f9fb4286e4b959378
Dayan A <codayanabdulla@gmail.com>
2025-01-27 08:26:22 -0500

    Opions made

---
+++ Options.swift
@@ -0,0 +1,43 @@
+//
+//  Options.swift
+//  RTR Challenge 5
+//
+//  Created by Dayan Abdulla on 1/27/25.
+//
+
+import SwiftUI
+
+struct Options: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .fill(Color.red)
+            .frame(width: 200, height: 50)
+            .overlay(
+                Text("Option 1")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30, weight: .bold))
+            )
+        
+        RoundedRectangle(cornerRadius: 15)
+            .fill(Color.red)
+            .frame(width: 200, height: 50)
+            .overlay(
+                Text("Option 2")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30, weight: .bold))
+            )
+        
+        RoundedRectangle(cornerRadius: 15)
+            .fill(Color.red)
+            .frame(width: 200, height: 50)
+            .overlay(
+                Text("Option 3")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30, weight: .bold))
+            )
+    }
+}
+
+#Preview {
+    Options()
+}
