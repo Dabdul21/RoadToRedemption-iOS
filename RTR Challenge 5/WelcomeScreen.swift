d7584caa00ecdb97c9ba7c99e35a30e117416640
Dayan A <codayanabdulla@gmail.com>
2025-02-04 08:24:32 -0500

    foxed

--- RTR Challenge 5/WelcomeScreen.swift
+++ RTR Challenge 5/WelcomeScreen.swift
@@ -5,26 +5,29 @@
 //  Created by Dayan Abdulla on 1/24/25.
 //
 
+
+//add background music
+
 import SwiftUI
 
 struct WelcomeScreen: View {
     var body: some View {
         NavigationStack{
         ZStack{
-            Image("W") // Replace with your image asset name or it wont show img
+            Image("NewBG") // Replace with your image asset name or it wont show img
                     .resizable()
                     .scaledToFill()
                     .frame(width: 459, height: 500) // Adjust width and height as needed later
                     .ignoresSafeArea()
-                    .offset(y: -100) // Move the image higher up on the screen
+                    .offset(y: -130) // Move the image higher up on the screen
             
             //delete this once u fix img
             //also make img cleaner
             //ONLY TEMP DELEYE THIS
             RoundedRectangle(cornerRadius: 15)
                 .fill(Color.black)
-                .offset(y: 375) // Move the image higher up on the screen
-                .frame(width: 500, height: 100)
+                .offset(y: 490) // Move the image higher up on the screen
+                .frame(width: 500, height: 250)
             
             VStack{
                 NavigationLink{
