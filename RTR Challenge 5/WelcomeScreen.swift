7dab3c7211c3598445cb0404a2d35b7cd97d8783
Dayan A <codayanabdulla@gmail.com>
2025-02-08 03:47:23 -0500

    Fixed oritentation bug, display issue, added music, and fixed welcome screen. May cause Preview probelms due to audio. Just chnage the #preview to ContentView if broken

--- RTR Challenge 5/WelcomeScreen.swift
+++ RTR Challenge 5/WelcomeScreen.swift
@@ -1,55 +1,40 @@
-//
-//  WelcomeScreen.swift
-//  RTR Challenge 5
-//
-//  Created by Dayan Abdulla on 1/24/25.
-//
-
-
-//add background music
-
 import SwiftUI
 
 struct WelcomeScreen: View {
+    @StateObject var audioManager = AudioManager.shared
+    @State private var navigateToNextScreen = false // Track navigation state
+
     var body: some View {
-        NavigationStack{
-        ZStack{
-            Image("NewBG") // Replace with your image asset name or it wont show img
+        NavigationStack {
+            ZStack {
+                Image("BGG")
                     .resizable()
-                    .scaledToFill()
-                    .frame(width: 459, height: 500) // Adjust width and height as needed later
+                    .scaledToFit()
                     .ignoresSafeArea()
-                    .offset(y: -130) // Move the image higher up on the screen
-            
-            //delete this once u fix img
-            //also make img cleaner
-            //ONLY TEMP DELEYE THIS
-            RoundedRectangle(cornerRadius: 15)
-                .fill(Color.black)
-                .offset(y: 490) // Move the image higher up on the screen
-                .frame(width: 500, height: 250)
-            
-            VStack{
-                NavigationLink{
-                    ScreenOne()
-                } label:{
+                    .frame(width: 450, height: 1190)
+
+                VStack {
                     RoundedRectangle(cornerRadius: 15)
                         .fill(Color.black)
                         .frame(width: 200, height: 50)
                         .overlay(
                             Text("PLAY")
                                 .foregroundColor(.white)
-                                .font(.system(size: 30, weight: .bold))
-                        )
-                    }
-                .isDetailLink(false) // Disables the default back button behavior (DONT REMOVE UNLESS U WANT A BACKBUTTON ON SCREEN ONE)
+                                .font(.system(size: 30, weight: .bold)))
                 }
+                .offset(y: -100) // Increase negative value to move it higher
+
+            }
+            .onTapGesture {
+                navigateToNextScreen = true // Trigger navigation on tap
+            }
+            .navigationDestination(isPresented: $navigateToNextScreen) {
+                ScreenOne() // Navigate to ScreenOne on tap
             }
         }
     }
 }
 
-
 #Preview {
-    WelcomeScreen()
+    ContentView()
 }
